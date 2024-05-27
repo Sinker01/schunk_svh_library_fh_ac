2f9e587367458cdfb249db04ccdcfb6eca3a422c
Sven <sven.kretschmer@alumni.fh-aachen.de>
2024-05-27 10:07:03 +0200

    Added java project

---
+++ src/Finger.java
@@ -0,0 +1,62 @@
+public enum Finger {
+
+    THUMB_FLEXION(0),
+    THUMB_DISTAL(1),
+    INDEX_DISTAL(2),
+    INDEX_PROXIMAL(3),
+    MIDDLE_DISTAL(4),
+    MIDDLE_PROXIMAL(5),
+    RING(6),
+    PINKY(7),
+    SPREAD(8);
+
+    private final int index;
+
+    Finger(int index) {
+        this.index = index;
+    }
+
+    public boolean setPositionTarget(double target) {
+        return !(setPositionTarget(index, target) == 0);
+    }
+
+    public boolean setSpeed(double speed) {
+        return !(setSpeed(index, speed) == 0);
+    }
+
+    public boolean setMaxNewton(double maxNewton) {
+        return !(setMaxNewton(index, maxNewton) == 0);
+    }
+
+    public short getmA() {
+        return getmA(index);
+    }
+
+    public double getNewton() {
+        return getNewton(index);
+    }
+
+    public double getPosition() {
+        return getPosition(index);
+    }
+
+    static {
+        System.load("/home/sven/CLionProjects/svh_driver/c_project/cmake-build-debug/libsvh-library.so");
+        System.load("/home/sven/CLionProjects/svh_driver/c_project/cmake-build-debug/libsvh-serial.so");
+        initFiveFingerManager();
+    }
+
+    private static native void initFiveFingerManager();
+
+    private static native char setPositionTarget(int finger, double position);
+
+    private static native char setSpeed(int finger, double speed);
+
+    private static native char setMaxNewton(int finger, double newton);
+
+    private static native short getmA(int finger);
+
+    private static native double getNewton(int finger);
+
+    private static native double getPosition(int finger);
+}
